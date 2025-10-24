e2047082d151f73a0d8402829718a4a7d4e06068
RahulPatil <rahulrohidaspatil@gmail.com>
2025-10-24 10:28:03 +0200

    configure shadcn/ui and add sidebar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,50 @@
+"use cache"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { items } from "@/lib/sidebar-items"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+
+export default async function Layout({ children }: LayoutProps<"/">) {
+  return (
+    <SidebarProvider>
+      <Sidebar variant="inset" collapsible="icon">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem
+                  key={item.title}
+                  className={cn(
+                    item.disabled && "pointer-events-none opacity-50",
+                  )}
+                >
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <SidebarInset>
+        <SidebarTrigger type="button" />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
---
+++ lib/sidebar-items.ts
@@ -0,0 +1,33 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+export const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+    disabled: true,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+    disabled: true,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+    disabled: true,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+    disabled: true,
+  },
+]
